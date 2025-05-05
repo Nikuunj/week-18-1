75ade6f55898e4a4d0ed532320ebfe81a4988cce
Nikunj <mnikunj262@gmail.com>
2025-05-05 17:19:18 +0530

    fix clinet output dir, and user

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-console.log('hi there');
- 
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+
+async function main() {
+  const allUsers = await prisma.user.create({
+    data : {
+        Username: 'Alice',
+        email: 'adada@example.com',
+        password: '123'
+    }
+  });
+  console.log(allUsers);
+}
+
+main();
